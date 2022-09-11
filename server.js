b92395317161e90cdaa3cb2c328d4e4e11a632fe
Zoki91 <zmitrevski2@gmail.com>
2022-09-11 20:39:53 +1000

    heroku fix2

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const path = require('path')
 const session = require('express-session')
 const routes = require('./controller/index')
 const exphbs = require('express-handlebars')
-
+const PORT = process.env.PORT || 3000
 
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const app = express();
-const PORT = process.env.PORT || 3001;
+
 
 
 // import sequelize connection
 const sequelize = require('./config/connection')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
 
 
 // Create Sessions
